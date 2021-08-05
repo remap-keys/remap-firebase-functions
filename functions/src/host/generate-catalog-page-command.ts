a94be36a8693b45b8532887f753c950ef3137cd9
Yoichiro Tanaka <yoichiro6642@gmail.com>
2021-08-05 20:55:59 +0900

    Improve the logic to generate the index.html content for Keyboard Catalog page.

--- functions/src/host/generate-catalog-page-command.ts
+++ functions/src/host/generate-catalog-page-command.ts
@@ -1,6 +1,7 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
-import * as qs from 'qs';
+import * as axios from 'axios';
+import { parse } from 'node-html-parser';
 
 type IKeyboardDefinition = {
   id: string;
@@ -14,11 +15,6 @@ type IFetchDefinitionDocumentResult = {
   definition?: IKeyboardDefinition;
 };
 
-type IEscapeParam = {
-  value: string;
-  escape: boolean;
-};
-
 export default class GenerateCatalogPageCommand {
   protected db: admin.firestore.Firestore;
 
@@ -26,18 +22,23 @@ export default class GenerateCatalogPageCommand {
     this.db = db;
   }
 
+  private static async fetchRemapIndexHtml(): Promise<string> {
+    const response = await axios.default.get('https://remap-keys.app/');
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error(`Fetching index.html failed. status=${response.status}`);
+    }
+  }
+
   async execute(
     req: functions.Request,
     res: functions.Response
   ): Promise<void> {
+    const html = await GenerateCatalogPageCommand.fetchRemapIndexHtml();
+    const root = parse(html);
     const path = req.path;
-    const queryParams = req.query;
-    const referrer = req.get('Referrer');
-    if (referrer) {
-      queryParams.referrer = referrer;
-    }
-    const query = qs.stringify(queryParams, { addQueryPrefix: true });
-    const regexp = /^\/catalog\/([a-zA-Z0-9]+)/;
+    const regexp = /\/catalog\/([a-zA-Z0-9]+)/;
     const m = path.match(regexp);
     let definitionDocumentId = undefined;
     if (m) {
@@ -46,53 +47,30 @@ export default class GenerateCatalogPageCommand {
     if (definitionDocumentId) {
       const result = await this.fetchDefinitionDocument(definitionDocumentId);
       if (result.exists) {
-        res.send(
-          this.replacePlaceholders(INDEX_HTML, {
-            TITLE: {
-              value: `${result.definition!.name} - Remap`,
-              escape: true,
-            },
-            DESCRIPTION: {
-              value: result.definition!.description,
-              escape: true,
-            },
-            IMAGE: { value: result.definition!.image, escape: true },
-            URL: {
-              value: `https://remap-keys.app/catalog/${result.definition!.id}`,
-              escape: true,
-            },
-            REDIRECT: {
-              value: this.createRedirectUrl(path, query),
-              escape: false,
-            },
-          })
-        );
-      } else {
-        res.send(
-          this.replacePlaceholders(INDEX_HTML, {
-            ...STANDARD_OGP,
-            REDIRECT: {
-              value: this.createRedirectUrl(path, query),
-              escape: false,
-            },
-          })
+        const title = root.querySelector('title');
+        title.set_content(`${result.definition!.name} - Remap`);
+        const ogTitle = root.querySelector('meta[property="og:title"]');
+        ogTitle.setAttribute('content', `${result.definition!.name} - Remap`);
+        if (result.definition!.description) {
+          const description = root.querySelector('meta[name="description"]');
+          description.setAttribute('content', result.definition!.description);
+          const ogDescription = root.querySelector(
+            'meta[property="og:description"]'
+          );
+          ogDescription.setAttribute('content', result.definition!.description);
+        }
+        if (result.definition!.image) {
+          const ogImage = root.querySelector('meta[property="og:image"]');
+          ogImage.setAttribute('content', result.definition!.image);
+        }
+        const ogUrl = root.querySelector('meta[property="og:url"]');
+        ogUrl.setAttribute(
+          'content',
+          `https://remap-keys.app/catalog/${result.definition!.id}`
         );
       }
-    } else {
-      res.send(
-        this.replacePlaceholders(INDEX_HTML, {
-          ...STANDARD_OGP,
-          REDIRECT: {
-            value: this.createRedirectUrl(path, query),
-            escape: false,
-          },
-        })
-      );
     }
-  }
-
-  createRedirectUrl(path: string, query: string): string {
-    return `https://remap-keys.app/_${path.substring(1)}${query}`;
+    res.send(root.toString());
   }
 
   async fetchDefinitionDocument(
@@ -108,7 +86,7 @@ export default class GenerateCatalogPageCommand {
       if (documentSnapshot.exists) {
         let description = documentSnapshot.data()!.description;
         if (description) {
-          description = this.curStringLength(description, 120);
+          description = this.cutStringLength(description, 120);
         }
         return {
           exists: true,
@@ -132,134 +110,11 @@ export default class GenerateCatalogPageCommand {
     }
   }
 
-  curStringLength(source: string, length: number): string {
+  cutStringLength(source: string, length: number): string {
     let trimmed = source.slice(0, length);
     if (source.length !== trimmed.length) {
       trimmed = `${trimmed}...`;
     }
     return trimmed;
   }
-
-  replacePlaceholders(
-    template: string,
-    params: { [p: string]: IEscapeParam }
-  ): string {
-    let result = template;
-    for (const key of Object.keys(params)) {
-      let param = params[key];
-      if (!param) {
-        param = STANDARD_OGP[key];
-      }
-      result = result
-        .split(`%${key}%`)
-        .join(param.escape ? this.escapeHtml(param.value) : param.value);
-    }
-    return result;
-  }
-
-  escapeHtml(text: string): string {
-    const map: { [p: string]: string } = {
-      '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      '"': '&quot;',
-      "'": '&#039;',
-    };
-    return text.replace(/[&<>"']/g, (m) => map[m]);
-  }
 }
-
-const STANDARD_OGP: { [p: string]: IEscapeParam } = {
-  TITLE: { value: 'Remap', escape: true },
-  DESCRIPTION: {
-    value:
-      'Remap allows you to configure key mappings and lighting of your keyboard with QMK firmware in Web Browser.',
-    escape: true,
-  },
-  IMAGE: { value: 'https://remap-keys.app/ogp_image.png', escape: true },
-  URL: { value: 'https://remap-keys.app', escape: true },
-};
-
-const INDEX_HTML = `
-<!DOCTYPE html>
-<html lang="en">
-  <head prefix="og: http://ogp.me/ns#">
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag('js', new Date());
-    </script>
-
-    <meta charset="utf-8" />
-    <link rel="icon" href="https://remap-keys.app/favicon.ico" />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    />
-
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <meta name="theme-color" content="#000000" />
-    <meta property="og:type" content="website" />
-    <meta property="og:site_name" content="Remap" />
-
-    <meta property="og:title" content="%TITLE%" data-rh="true" />
-    <meta
-      name="description"
-      content="%DESCRIPTION%"
-      data-rh="true"
-    />
-    <meta
-      property="og:description"
-      content="%DESCRIPTION%"
-      data-rh="true"
-    />
-    <meta property="og:url" content="%URL%" data-rh="true" />
-    <meta
-      property="og:image"
-      content="%IMAGE%"
-      data-rh="true"
-    />
-
-    <title>%TITLE%</title>
-
-    <link rel="apple-touch-icon" href="https://remap-keys.app/logo192.png" />
-    <link rel="manifest" href="https://remap-keys.app/manifest.json" />
-    <script type="application/ld+json">
-      {
-        "@context": "https://schema.org",
-        "@type": "WebApplication",
-        "name": "Remap",
-        "description": "Remap allows you to configure key mappings and lighting of your keyboard with QMK firmware in Web Browser.",
-        "url": "https://remap-keys.app",
-        "image": "https://remap-keys.app/ogp_image.png",
-        "applicationCategory": "UtilitiesApplication",
-        "operatingSystem": "Chromium-based Web Browsers",
-        "offers": {
-          "@type": "Offer",
-          "price": "0"
-        },
-        "browserRequirements": "requires Chrome or Edge 89 or higher",
-        "featureList": "Customize a key mapping of your keyboard, Save/restore/share your key mappings with other users, Test key switch matrix, Control LEDs",
-        "releaseNotes": "https://github.com/remap-keys/remap/wiki/Release-notes",
-        "keywords": "remap,keyboard,keymap,qmk,via,customize,led,diy,webhid"
-      }
-    </script>
-  </head>
-  <body>
-    <script type="text/javascript">
-      setTimeout(() => {
-        location.href = "%REDIRECT%";
-      }, 0);
-    </script>
-  </body>
-</html>
-`;
